49e70b934050c82fe695de57865c82b8e1919d86
Tejas H <tejas007tez@gmail.com>
2020-08-04 18:03:01 +0530

    🎉Initial setup commit

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
-const express = require('express')
-const next = require('next')
+const envVars = require("dotenv").config();
+const express = require("express");
+const next = require("next");
+const bodyParser = require("body-parser");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+if (envVars.error) {
+  throw envVars.error;
+}
+
+const PORT = parseInt(process.env.PORT, 10) || 5050;
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express()
+  const server = express();
 
-  server.get('/a', (req, res) => {
-    return app.render(req, res, '/a', req.query)
-  })
+  server.use(bodyParser.urlencoded({ extended: true }));
+  server.use(bodyParser.json());
 
-  server.get('/b', (req, res) => {
-    return app.render(req, res, '/b', req.query)
-  })
+  // server.get("/a", (req, res) => {
+  //   return app.render(req, res, "/a", req.query);
+  // });
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+  // server.get("/b", (req, res) => {
+  //   return app.render(req, res, "/b", req.query);
+  // });
 
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(PORT, err => {
+    if (err) {
+      throw err;
+    }
+    // eslint-disable-next-line no-console
+    console.log(`> Ready on http://localhost:${PORT}`);
+  });
+});
